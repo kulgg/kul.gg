bb310504d8c94e5fb0d2640d61a23727defdb242
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-21 20:54:58 +0200

    Containerize the layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
+		container: {
+			center: true,
+			screens: {
+				sm: "100%",
+				md: "100%",
+				lg: "800px",
+				xl: "800px",
+			},
+		},
 	},
 	plugins: [],
-}
+};
