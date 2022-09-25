5c48f6e323c9ef341dee5f485e6fe26ac11aeb6e
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-25 22:58:10 +0200

    Improve dynamic header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Header = (): JSX.Element => {
 	const [headerIsVisible, setHeaderIsVisible] = useState(false);
 
 	useEffect(() => {
 		const adjustVisibility = () => {
-			setHeaderIsVisible(window.pageYOffset > 420);
+			setHeaderIsVisible(window.pageYOffset > screen.height / 2 - 50);
 		};
 
 		addEventListener("scroll", adjustVisibility);
@@ -18,7 +18,7 @@ const Header = (): JSX.Element => {
 	}
 
 	return (
-		<header className="z-50 sticky top-0 bg-gray-900 shadow-md">
+		<header className="z-50 sticky top-0 bg-gray-900 shadow-md px-1">
 			<div className="flex justify-between items-center shadow-sm h-12 container">
 				<h1 className="text-xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-pink-200 via-purple-200 to-orange-200 animate-gradient-text">
 					<a
