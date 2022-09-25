dfc371b1eb11fe355bfd199a8fa55de6c88347d2
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-25 15:02:52 +0200

    Add technologies section

---
+++ src/data/technologies.ts
@@ -0,0 +1,13 @@
+const languages: string[] = ["TypeScript", "JavaScript", "C#", "Python", "SQL"];
+const frameworks: string[] = [
+	"React",
+	"Next.js",
+	"Tailwind CSS",
+	".NET 6",
+	"Entity Framework Core",
+	"Astro",
+	"Prisma",
+];
+const tools: string[] = ["Docker", "git", "GitHub", "Azure DevOps", "Vercel"];
+
+export { languages, frameworks, tools };
