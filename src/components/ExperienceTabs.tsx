ab25ad2f3c015aeae03d14cc47f5fb2ad2eece3f
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-25 21:46:56 +0200

    Add experience tabs

---
+++ src/components/ExperienceTabs.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const tabs: string[] = ["Education", "Technologies"];
+
+const ExperienceTabs = ({
+	children,
+}: {
+	children: JSX.Element;
+}): JSX.Element => {
+	const [activeTab, setActiveTab] = useState("Education");
+
+	return (
+		<div>
+			<div className="text-sm font-medium text-center border-b text-gray-400 border-gray-700">
+				<ul className="flex flex-wrap -mb-px">
+					{tabs.map((tab) => {
+						return tab === activeTab ? (
+							<li className="mr-2">
+								<a
+									className="cursor-pointer inline-block p-4 rounded-t-lg border-b-2 active text-blue-400 border-blue-400"
+									aria-current="page"
+								>
+									{tab}
+								</a>
+							</li>
+						) : (
+							<li className="mr-2" onClick={() => setActiveTab(tab)}>
+								<a className="cursor-pointer inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:border-gray-300 hover:text-gray-300">
+									{tab}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			{activeTab === "Education" && <div>Education</div>}
+		</div>
+	);
+};
+
+export default ExperienceTabs;
