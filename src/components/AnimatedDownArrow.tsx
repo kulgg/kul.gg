a2132e43114d0eccc57e884ca172d985a2078f21
kulgg <75735874+kulgg@users.noreply.github.com>
2022-09-26 22:48:22 +0200

    Add animated down arrow

---
+++ src/components/AnimatedDownArrow.tsx
@@ -0,0 +1,34 @@
+import { LazyMotion, domAnimation, m } from "framer-motion";
+
+export default function AnimatedDownArrow(): JSX.Element {
+	return (
+		<LazyMotion features={domAnimation}>
+			<m.div
+				animate={{ y: [15, 0, 15] }}
+				transition={{
+					repeat: Infinity,
+					duration: 3,
+					mass: 2,
+					stiffness: 10,
+					bounce: 1,
+				}}
+				className=""
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth="1.5"
+					stroke="currentColor"
+					className="w-6 h-6"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+					/>
+				</svg>
+			</m.div>
+		</LazyMotion>
+	);
+}
