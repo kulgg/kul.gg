7119b24bd213324db4383d7d162cbfd33b7c642f
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-03 12:44:31 +0200

    Refactor header

--- src/layouts/DynamicHeader.tsx
+++ src/layouts/DynamicHeader.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Header from "./Header";
 
 export default function DynamicHeader(): JSX.Element {
   const [headerIsVisible, setHeaderIsVisible] = useState(false);
@@ -13,28 +14,8 @@ export default function DynamicHeader(): JSX.Element {
   }, []);
 
   if (!headerIsVisible) {
-    return <div></div>;
+    return <></>;
   }
 
-  return (
-    <header className="z-50 sticky top-0 bg-gray-900 shadow-md px-1">
-      <div className="flex justify-between items-center shadow-sm h-12 container">
-        <h1 className="text-xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-pink-200 via-purple-200 to-orange-200 animate-gradient-text">
-          <a
-            href="/"
-            className="pb-2 text-xl font-bold text-center text-transparent bold-text bg-clip-text bg-gradient-to-r from-pink-200 via-purple-200 to-orange-200 animate-gradient-text"
-          >
-            kul<span className="text-blue-500">.</span>gg
-          </a>
-        </h1>
-        <nav>
-          <ul className="flex justify-start gap-2 items-center text-md">
-            <li>
-              <a href="/blog">Blog</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+  return <Header />;
 }
