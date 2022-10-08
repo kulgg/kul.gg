90ee966f13f32a4575e21947e900bcdd1126430d
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-08 18:19:38 +0200

    Add season emoji to farewell message

--- src/components/FarewellMessage.tsx
+++ src/components/FarewellMessage.tsx
@@ -14,24 +14,59 @@ function getDayName(date: Date): string {
   return days[date.getDay()];
 }
 
+function getSeasonEmoji(date: Date): string {
+  const spring = "🌱";
+  const summer = "☀️";
+  const fall = "🍂";
+  const winter = "❄️";
+
+  const monthIndex = date.getMonth();
+
+  if (monthIndex >= 2 && monthIndex < 5) {
+    return spring;
+  } else if (monthIndex >= 5 && monthIndex < 8) {
+    return summer;
+  } else if (monthIndex >= 8 && monthIndex < 11) {
+    return fall;
+  } else {
+    return winter;
+  }
+}
+
 export default function FarewellMessage(): JSX.Element {
   const [message, setMessage] = useState("");
+  const [isCursorBlinking, setIsCursorBlinking] = useState(true);
 
   useEffect(() => {
-    const msg = `Have a good ${getDayName(new Date())}!`;
+    const msg = `Have a good ${getDayName(new Date())} ${getSeasonEmoji(
+      new Date()
+    )}`;
 
-    const timeouts = [...msg].map((chr, i) =>
-      setTimeout(() => {
+    const msgChars = [...msg];
+
+    const timeouts = msgChars.map((chr, i) => {
+      return setTimeout(() => {
         setMessage((oldMsg) => [...oldMsg, chr].join(""));
-      }, 125 * (i + 1))
-    );
+      }, 125 * (i + 1));
+    });
+
+    const stopBlinkingTimeout = setTimeout(() => {
+      setIsCursorBlinking(false);
+    }, 125 * (msgChars.length + 10));
 
     return () => {
       timeouts.forEach((x) => clearTimeout(x));
+      clearTimeout(stopBlinkingTimeout);
     };
   }, []);
 
   return (
-    <div className="mb-6 text-center text-md blinking-cursor">{message}</div>
+    <div
+      className={`mb-6 text-center text-md ${
+        isCursorBlinking ? "blinking-cursor" : ""
+      }`}
+    >
+      {message}
+    </div>
   );
 }
